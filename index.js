9df3f5cb6c77ca2f4a2d5c4a3c2f5a668811a5bb
ndukwesamuel <39251068+ndukwesamuel@users.noreply.github.com>
2022-03-23 11:34:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// // 1: ask A user to register their email and password
+// let user_Data = []
+// alert('pls enter your email and password')
+// let user_email = prompt(' Enter email: ')
+// let user_password = prompt(' Enter password: ')
+// console.log(user_password.length)
+
+// console.log(user_Data);
+// user_Data.push(user_email)
+// user_Data.push(user_password)
+
+// console.log(usedr_Data);
+
+// // if(user_password >= 8 ){
+// //     console.log(user_email, user_password);
+// //     console.log('thanks for registratring ');
+
+// // }else{
+// //     console.log('password not secured ');
+// // }
+
+// let name = { sam: "kaka", age: 4 };
+let name = ["peter", "kaka"];
+let [fir] = name;
+console.log(fir);
